2ec76d98c68c40c89b7cd2494d49b08f65d6d92e
Henrique Salazar da Silva <henriquesalazards16@hotmail.com>
2025-03-21 13:55:49 -0300

    another att on visuals

--- index.js
+++ index.js
@@ -1,60 +1,18 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const bcrypt = require("bcryptjs");
-const mysql = require("mysql2");
-const cors = require("cors");
+let root = document.querySelector(':root')
+let body = document.querySelector('body')
 
-const app = express();
-const port = 3000;
-
-// Configurar middleware
-app.use(bodyParser.json());
-app.use(cors());
-
-// Conectar ao banco de dados MySQL
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "clone_ifood"
-});
-
-db.connect(err => {
-    if (err) {
-        console.error("Erro ao conectar ao banco de dados:", err);
-        return;
+document.getElementById('themeSwitcher').addEventListener('click', function(){
+    if(body.dataset.theme === 'light') {
+    root.style.setProperty('--color-primary', '#66b3ff')
+    root.style.setProperty('--color-primary-hover', '#3385cc')
+    root.style.setProperty('--color-background', 'rgba(18, 18, 18, 1)')
+    root.style.setProperty('--color-background-light', 'rgba(31, 31, 31, 0.9)')
+    root.style.setProperty('--color-text', '#e0e0e0')
+    root.style.setProperty('--color-error', '#ff4d4d')
+    root.style.setProperty('--color-shadow', 'rgba(0, 0, 0, 0.6)')
+    root.style.setProperty('--color-link', '#66b3ff')
+    root.style.setProperty('--color-img', '#ffffff')
+    body.dataset.theme = 'dark'
     }
-    console.log("Conectado ao banco de dados!");
-});
 
-// Rota para cadastrar um novo usuário
-app.post("/register", (req, res) => {
-    const { nome, sobrenome, email, celular, senha } = req.body;
-
-    // Verificar se o e-mail já existe
-    db.query("SELECT * FROM usuarios WHERE email = ?", [email], (err, result) => {
-        if (err) return res.status(500).json({ success: false, message: "Erro no servidor!" });
-
-        if (result.length > 0) {
-            return res.json({ success: false, message: "E-mail já cadastrado!" });
-        }
-
-        // Criptografar a senha
-        bcrypt.hash(senha, 10, (err, hashedPassword) => {
-            if (err) return res.status(500).json({ success: false, message: "Erro ao processar senha!" });
-
-            // Inserir o usuário no banco de dados
-            const query = "INSERT INTO usuarios (nome, sobrenome, email, celular, senha) VALUES (?, ?, ?, ?, ?)";
-            db.query(query, [nome, sobrenome, email, celular, hashedPassword], (err, result) => {
-                if (err) return res.status(500).json({ success: false, message: "Erro ao cadastrar usuário!" });
-
-                res.json({ success: true, message: "Cadastro realizado com sucesso!" });
-            });
-        });
-    });
-});
-
-// Iniciar o servidor
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-});
+})
